7c94aa43897f4402e19cfab8ab2a7a76e510e779
grim42 <ishaangt@gmail.com>
2019-09-16 21:52:14 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PersonalDetailsComponent } from 'src/app/personal-details/personal-details.component';
+import { WorkExperienceComponent } from 'src/app/work-experience/work-experience.component';
+import { SkillsComponent } from 'src/app/skills/skills.component';
+import { ContactDetailsComponent } from 'src/app/contact-details/contact-details.component';
+import { HomeComponent } from './home/home.component';
+import { WizardFinishedComponent } from './wizard-finished/wizard-finished.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path:"home", component: HomeComponent},
+  {path:"personal-details", component: PersonalDetailsComponent},
+  {path:"contact-details", component: ContactDetailsComponent},
+  {path:"skill-details", component: SkillsComponent},
+  {path:"work-details", component: WorkExperienceComponent},
+  {path:"finished", component: WizardFinishedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
